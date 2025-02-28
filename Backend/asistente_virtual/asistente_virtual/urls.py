5a2ca402cc79f6dd1ca9cf6b33108eedec90a35d
JeremiTC <jeremy.genial1@gmail.com>
2025-02-28 13:24:39 -0500

    Added Backend CRUD

--- Backend/asistente_virtual/asistente_virtual/urls.py
+++ Backend/asistente_virtual/asistente_virtual/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Gym_app project.
+URL configuration for asistente_virtual project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
